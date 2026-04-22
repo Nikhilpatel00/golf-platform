582a8338b36e1052c761685126812fd389e56021
Nikhil Patel <nikhilpatil67274@gmail.com>
2026-04-22 11:28:04 +0530

    final fix admin page clean

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
-}
-=======
 export default function AdminPage() {
-  return <div>Admin Page </div>;
+  return <div>Admin Page</div>;
 }
->>>>>>> 98da13d5453f64d3b18ac31620ba4822800ec9ec
